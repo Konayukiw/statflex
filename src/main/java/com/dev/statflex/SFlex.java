e0ba45dc12422391e28e5f1c881a1dbaa292a64d
Konayukiw <konayukiw@outlook.com>
2025-09-21 21:48:35 +0900

    version 1.01

--- src/main/java/com/dev/statflex/SFlex.java
+++ src/main/java/com/dev/statflex/SFlex.java
@@ -8,7 +8,7 @@ import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 public class SFlex {
     public static final String MODID = "statflex";
     public static final String NAME = "StatFlex";
-    public static final String VERSION = "1.0";
+    public static final String VERSION = "1.01";
 
     @Mod.EventHandler
     public void init(FMLInitializationEvent event) {
