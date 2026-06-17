34512ae72ee4486aee3714520ab2fe5406b6253d
Konayukiw <konayukiw@outlook.com>
2026-06-18 01:50:10 +0900

    Huge refactoring

--- src/main/java/com/konayuki/statflex/statflex.java
+++ src/main/java/com/konayuki/statflex/statflex.java
@@ -8,7 +8,7 @@ public class statflex {
 
     public static final String MODID = "statflex";
     public static final String NAME = "statflex";
-    public static final String VERSION = "2.02";
+    public static final String VERSION = "2.03";
 
     @Mod.EventHandler
     public void init(FMLInitializationEvent event) {
