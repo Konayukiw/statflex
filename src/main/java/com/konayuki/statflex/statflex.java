9f0bab709f5eb7b3c3084f340ff77a5f89653e81
Konayukiw <konayukiw@outlook.com>
2026-06-26 01:53:41 +0900

    Completed debugging for Denicker

--- src/main/java/com/konayuki/statflex/statflex.java
+++ src/main/java/com/konayuki/statflex/statflex.java
@@ -8,7 +8,7 @@ public class statflex {
 
     public static final String MODID = "statflex";
     public static final String NAME = "statflex";
-    public static final String VERSION = "2.12";
+    public static final String VERSION = "2.13";
 
     @Mod.EventHandler
     public void init(FMLInitializationEvent event) {
